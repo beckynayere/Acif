d08686f69238c783762e5b249388d852dbefe21c
beckynayere <rebecca.nayere@gmail.com>
2025-02-07 19:53:09 +0300

    create campaign shema

--- backend/src/campaigns/schemas/campaign.schema/campaign.schema.ts
+++ backend/src/campaigns/schemas/campaign.schema/campaign.schema.ts
@@ -1,1 +1,25 @@
-export class CampaignSchema {}
+// export class CampaignSchema {}
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type CampaignDocument = Campaign & Document;
+
+@Schema({ timestamps: true })
+export class Campaign {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  deadline: Date;
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }] })
+  influencers: Types.ObjectId[];
+}
+
+export const CampaignSchema = SchemaFactory.createForClass(Campaign);
+
+CampaignSchema.index({ title: 1 }, { unique: true });
