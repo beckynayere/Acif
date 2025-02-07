0d0794fd46f10868c1a3f55297e6c54d89e7e2e9
beckynayere <rebecca.nayere@gmail.com>
2025-02-07 15:06:38 +0300

    create auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule} from '@nestjs/config'; 
+ 
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule],
+    ConfigModule.forRoot(),
+      MongooseModule.forRoot(process.env.MONGO_URI),
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
