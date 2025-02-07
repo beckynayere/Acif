0ee0f44a1c1655f98e09588e03ddead64ee2fa7b
beckynayere <rebecca.nayere@gmail.com>
2025-02-07 15:18:14 +0300

    manually install .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule} from '@nestjs/config'; 
- 
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
-    ConfigModule.forRoot(),
-      MongooseModule.forRoot(process.env.MONGO_URI),
+  imports: [
+    ConfigModule.forRoot(), // Load environment variables first
+    MongooseModule.forRoot(process.env.MONGO_URI), 
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
