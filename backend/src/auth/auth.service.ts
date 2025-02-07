5b169a4b23a3f22ea5e493d5cf0adf2250d78233
beckynayere <rebecca.nayere@gmail.com>
2025-02-08 01:47:31 +0300

    create jwt.strategy.ts

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,26 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/users/users.entity';
+import { AccessToken } from './types/AccessToken';
+import { UsersService } from 'src/users/users.service';
+import { RegisterRequestDto } from './dtos/register-request.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+  async validateUser(email: string, password: string): Promise<User> {
+    const user: User = await this.usersService.findOneByEmail(email);
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+    const isMatch: boolean = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+      throw new BadRequestException('Password does not match');
+    }
+    return user;
   }
-
-  findAll() {
-    return `This action returns all auth`;
+  async login(user: User): Promise<AccessToken> {
+    const payload = { email: user.email, id: user.id };
+    return { access_token: this.jwtService.sign(payload) };
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async register(user: RegisterRequestDto): Promise<AccessToken> {
+    const existingUser = await this.usersService.findOneByEmail(user.email);
+    if (existingUser) {
+      throw new BadRequestException('email already exists');
+    }
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    const newUser: User = { ...user, password: hashedPassword };
+    await this.usersService.create(newUser);
+    return this.login(newUser);
   }
 }
